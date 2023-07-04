f0251fa83f941f4c65bbe65eb898e2a508f4159c
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-04 11:22:22 +0530

    order controller finish and starting frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/layout/Header.js";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+	return (
+		<BrowserRouter>
+      <Header />
+    </BrowserRouter>
+	)
+}
+
+export default App
